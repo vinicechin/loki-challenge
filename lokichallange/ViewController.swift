f0f42503eceef2250a4a6735bb8b9dcc59fa1971
Vinicius Cechin <vinicechin@gmail.com>
2019-10-16 13:26:27 +0200

    Initial commit

---
+++ lokichallange/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lokichallange
+//
+//  Created by Gabriella Barbieri on 16/10/19.
+//  Copyright © 2019 Vinicius Cechin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
